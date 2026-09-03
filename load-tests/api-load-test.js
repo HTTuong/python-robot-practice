eb24bcf89de828040a6bb8a9ec38bc2f848cf678
Tuong Hoang <httuong.2705@gmail.com>
2026-09-03 23:57:08 +0300

    Add thresholds to  API load test

--- load-tests/api-load-test.js
+++ load-tests/api-load-test.js
@@ -4,6 +4,10 @@ import { check, sleep } from 'k6';
 export const options = {
   vus: 10,          // 10 "virtual user"
   duration: '30s',  // running in 30s
+  thresholds: {
+    http_req_duration: ['p(95)<500'],  // 95% request phải nhanh hơn 500ms
+    http_req_failed: ['rate<0.01'],    // tỷ lệ lỗi phải dưới 1%
+  }
 };
 
 export default function () {
